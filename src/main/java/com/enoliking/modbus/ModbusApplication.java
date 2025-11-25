f5f243fbb7aaddd6b0d6cedc18fe48de3a5b0eb6
István Kiss <istvan.kiss3@tesco.com>
2025-11-25 18:59:50 +0100

    first commit

---
+++ src/main/java/com/enoliking/modbus/ModbusApplication.java
@@ -0,0 +1,13 @@
+package com.enoliking.modbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enoliking/modbus/ModbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enoliking.modbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
